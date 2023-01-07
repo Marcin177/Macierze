07ddd92ba0032d0238d83527047ec46cb6bac559
Marcin177 <gonciarz.m177@gmail.com>
2023-01-07 22:37:29 +0100

    szablon

--- Macierze/Macierze.cpp
+++ Macierze/Macierze.cpp
@@ -1,6 +1,51 @@
 ﻿using namespace std;
 
 #include <iostream>
+//templete<class T>
+/*class Macierz
+{
+private:
+
+public:
+	Macierz() :; //domyślny
+	Macierz() :; //z parametrami
+	Macierz() :; //kopiujący
+	Macierz() :; //
+	~Macierz(); //destruktor
+
+	Macierz& operator+(Macierz& a);				//
+	Macierz operator++();						//
+	Macierz operator++(int);					//  operacje na macierzach
+	Macierz operator+(double a);				//
+	Macierz operator+(double a, Macierz& b);	//
+	Macierz& operator-(Macierz& a);				//
+	Macierz operator--();						//
+	Macierz operator--(int);					//
+	Macierz operator-(double a);				//
+	Macierz operator-(double a; Macierz& b);	//
+};
+
+class Import_Export 
+{
+private:
+
+public:
+	Import_Export() :;
+	Import_Export() :;
+	~Import_Export();
+
+};
+
+class Adapret
+{
+private:
+
+public:
+	Adapret();
+	~Adapret();
+
+};
+*/
 
 int main()
 {
